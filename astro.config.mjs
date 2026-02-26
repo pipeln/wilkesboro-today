d543aff269ea90101ee87f8bc1e195ce52b0852b
Wilkesboro Today <admin@wilkesborotoday.com>
2026-02-26 10:11:58 +0800

    Temporarily disable sitemap to fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+// import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wilkesboro-today.pages.dev',
   integrations: [
     tailwind(),
-    sitemap({
-      filter: (page) => !page.includes('/admin'),
-      customPages: [
-        'https://wilkesboro-today.pages.dev/',
-        'https://wilkesboro-today.pages.dev/events/',
-      ],
-    }),
+    // sitemap(),
   ],
   output: 'static',
   build: {
